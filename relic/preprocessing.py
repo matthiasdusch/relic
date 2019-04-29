c0378b556339688920fed7a570a7c3b7221a20cd
Matthias Dusch <matthias.dusch@uibk.ac.at>
2019-04-29 15:14:49 +0200

    integrate leclercq and JJA temperature

--- relic/preprocessing.py
+++ relic/preprocessing.py
@@ -7,6 +7,8 @@ from oggm.core.climate import compute_ref_t_stars
 from oggm.workflow import execute_entity_task
 from oggm import entity_task
 
+from relic.process_leclercq import download_leclercq, select_my_glaciers
+
 import logging
 log = logging.getLogger(__name__)
 
@@ -61,6 +63,21 @@ def configure(workdir, glclist, glena_factor=1.5, baselineclimate='HISTALP',
 
         # cfg.PARAMS['run_mb_calibration'] = True
         # compute_ref_t_stars(gdirs)
+
+    if baselineclimate == 'HISTALP_JJA_TEMP':
+        cfg.PARAMS['baseline_climate'] = 'HISTALP'
+        execute_entity_task(tasks.process_histalp_data, gdirs)
+
+        cfg.PARAMS['baseline_climate'] = baselineclimate
+        execute_entity_task(annual_temperature_from_summer_temp, gdirs)
+
+    if baselineclimate == 'HISTALP_JJA_TEMP_AND_ANNUAL_PCP':
+        cfg.PARAMS['baseline_climate'] = 'HISTALP'
+        execute_entity_task(tasks.process_histalp_data, gdirs)
+
+        cfg.PARAMS['baseline_climate'] = baselineclimate
+        execute_entity_task(histalp_annual_mean, gdirs, y0=y0, years=years)
+        execute_entity_task(annual_temperature_from_summer_temp, gdirs)
 
     execute_entity_task(tasks.local_t_star, gdirs)
     execute_entity_task(tasks.mu_star_calibration, gdirs)
@@ -118,3 +135,73 @@ def histalp_annual_mean(gdir, y0=None, years=None):
            'baseline_hydro_yr_0': time[0].year + 1,
            'baseline_hydro_yr_1': time[-1].year}
     gdir.write_pickle(out, 'climate_info')
+
+
+@entity_task(log, writes=['climate_monthly', 'climate_info'])
+def annual_temperature_from_summer_temp(gdir, y0=1950, years=30):
+    """
+
+    Parameters
+    ----------
+    gdir
+    y0
+    years
+
+    Returns
+    -------
+
+    """
+
+    # read original histalp climate monthly
+    cm = xr.open_dataset(gdir.get_filepath('climate_monthly'))
+    time = pd.DatetimeIndex(cm.time.values)
+    temp = cm.temp.copy()
+    prcp = cm.prcp.values
+    hgt = cm.ref_hgt
+    lon = cm.ref_pix_lon
+    lat = cm.ref_pix_lat
+    igrad = None
+    if cfg.PARAMS['temp_use_local_gradient']:
+        raise RuntimeError('Have to think about it')
+
+    # get temeratures of reference period
+    tref = temp.loc[pd.to_datetime('%d' % y0):
+                    pd.to_datetime('%d' % (y0+years))][:-1].copy()
+
+    # monthly mean temperature of reference period
+    ref_monthly = tref.groupby('time.month').mean()
+    # summer mean temperature of reference period -> one value
+    ref_jja = ref_monthly.loc[(ref_monthly.month >= 6) &
+                              (ref_monthly.month <= 8)].mean().values
+
+    # remove non-summer temperatures
+    temp.loc[(temp.time.dt.month > 8) | (temp.time.dt.month < 6)] = np.nan
+
+    # yearly series of summer mean temeratures
+    tmean = temp.groupby('time.year').mean()
+
+    # and create monthly time series with yearly bias
+    for y in tmean:
+        bias = ref_jja - y.values
+        temp.loc[temp.time.dt.year == y.year] = ref_monthly.loc[
+            temp.loc[temp.time.dt.year == y.year].time.dt.month] - bias
+
+    temp = temp.values
+
+    # write climate data
+    gdir.write_monthly_climate_file(time, prcp, temp, hgt, lon, lat,
+                                    gradient=igrad)
+
+    # metadata
+    out = {'baseline_climate_source': 'HISTALP jja temps',
+           'baseline_hydro_yr_0': time[0].year + 1,
+           'baseline_hydro_yr_1': time[-1].year}
+    gdir.write_pickle(out, 'climate_info')
+
+
+def get_leclercq_observations():
+    meta_all, data_all = download_leclercq()
+    meta, data = select_my_glaciers(meta_all, data_all)
+    meta['first'] = meta['first'].astype(int)
+    data.columns = data.columns.astype(int)
+    return meta, data
